ccd19c2023c22f7397f617dac249b5a3246ccdc1
BrundevCoder <brunodevdrums@gmail.com>
2026-07-12 00:57:16 +0100

    add: random jokes with reroll button

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const showTime = document.getElementById("time");
+const showJoke = document.getElementById("jokeMessage");
+const rerrolButton = document.getElementById("reroll-joke-btn");
+
+const jokeAPIUrl = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 
 function updateTime() {
   let newDate = new Date();
@@ -12,8 +16,22 @@ function updateTime() {
   showTime.innerText = timeString
 }
 
+function getJoke() {
+
+  fetch(jokeAPIUrl)
+    .then(response => response.json())
+    .then( (json) => {
+      let joke = json.joke;
+      showJoke.innerText = joke
+    })
+}
+
 setInterval(() => {
-  updateTime()
+  updateTime();
 }, 1000);
 
-updateTime()
+updateTime();
+
+rerrolButton.addEventListener("click", getJoke);
+
+getJoke();
